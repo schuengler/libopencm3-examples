5b5c73e6847fe34952b7ba294404c5836eb0d2fa
schuengler <schuengel.max@live.de>
2025-05-04 19:48:15 +0200

    Improve spi-usart-dma example by fully supporting dma

--- examples/stm32/f4/nucleo-f401re/spi-usart-dma/adxl345.h
+++ examples/stm32/f4/nucleo-f401re/spi-usart-dma/adxl345.h
@@ -3,19 +3,15 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/spi.h>
 
-static void adxl345_cs_low(void)  { gpio_clear(GPIOA, GPIO4); }
-static void adxl345_cs_high(void) { gpio_set(GPIOA, GPIO4); }
+#define CS_PORT GPIOA
+#define CS_PIN  GPIO4
 
-//static uint8_t adxl345_read_reg(uint32_t spi, uint8_t reg) 
-//{
-//    adxl345_cs_low();
-//    spi_send(spi, 0x80 | reg);           // Read flag
-//    uint8_t val = spi_read(spi);
-//    spi_send(spi, 0x00);                 // Dummy write
-//    val = spi_read(spi);
-//    adxl345_cs_high();
-//    return val;
-//}
+#define ADXL345_REG_POWER_CTL  0x2D
+#define ADXL345_REG_DATA_X0    0x32
+#define ADXL345_MEASURE        0x08
+
+static void adxl345_cs_low(void)  { gpio_clear(CS_PORT, CS_PIN); }
+static void adxl345_cs_high(void) { gpio_set(CS_PORT, CS_PIN); }
 
 /// @brief Write given register with given value through given spi
 /// @param spi 
@@ -36,29 +32,5 @@ static void adxl345_write_reg(uint32_t spi, uint8_t reg, uint8_t val)
 static void adxl345_init(uint32_t spi) 
 {
     // POWER_CTL = 0x08 (Measurement mode)
-    adxl345_write_reg(spi, 0x2D, 0x08);
-}
-
-/// @brief Read xyz accelations through the given spi 
-/// @param spi SPI identifier
-/// @param x[out] Acceleration in x-diemension
-/// @param y[out] Acceleration in y-diemension
-/// @param z[out] Acceleration in z-diemension
-static void adxl345_read_xyz(uint32_t spi, int16_t *x, int16_t *y, int16_t *z)
-{
-    uint8_t raw[6];
-
-    adxl345_cs_low();
-    spi_send(spi, 0xC0 | 0x32); // Read multiple starting at 0x32
-    spi_read(spi);
-
-    for (int i = 0; i < 6; i++) {
-        spi_send(spi, 0x00);
-        raw[i] = spi_read(spi);
-    }
-    adxl345_cs_high();
-
-    *x = (int16_t)((raw[1] << 8) | raw[0]);
-    *y = (int16_t)((raw[3] << 8) | raw[2]);
-    *z = (int16_t)((raw[5] << 8) | raw[4]);
+    adxl345_write_reg(spi, ADXL345_REG_POWER_CTL, ADXL345_MEASURE);
 }
--- examples/stm32/f4/nucleo-f401re/spi-usart-dma/spi-usart-dma.c
+++ examples/stm32/f4/nucleo-f401re/spi-usart-dma/spi-usart-dma.c
@@ -1,9 +1,10 @@
 #include <libopencm3/cm3/nvic.h>
-#include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/dma.h>
-#include <libopencm3/stm32/spi.h>
 #include <libopencm3/stm32/rcc.h>
+#include <libopencm3/stm32/gpio.h>
+#include <libopencm3/stm32/spi.h>
+#include <libopencm3/stm32/dma.h>
 #include <libopencm3/stm32/usart.h>
+#include <libopencm3/cm3/systick.h>
 
 #include <stdarg.h>
 #include <stdio.h>
@@ -11,10 +12,14 @@
 
 #include "adxl345.h"
 
-#define ADXL_ADDRESS (0x53)
+#define CS_PORT GPIOA
+#define CS_PIN  GPIO4
 
-#define NDMA_CIRCULAR
+#define ADXL345_REG_POWER_CTL  0x2D
+#define ADXL345_REG_DATA_X0    0x32
+#define ADXL345_MEASURE        0x08
 
+// DMA1 for usart2
 // DMA1 request mapping (table 43, STM32F4x reference manual) 
 // specifies channel 4 stream 6 for USART2_TX
 #define DMA_DEMO_STREAM DMA_STREAM6
@@ -27,13 +32,15 @@ typedef char DmaDataType;
 /// Function declarations
 ///
 static void clock_setup(void);
-static void dma1_setup(DmaDataType* data, size_t size);
+static void gpio_setup(void);
 static void usart2_setup(void);
 static void spi1_setup(void);
-static void gpio_setup(void);
+static void dma1_usart2_setup(DmaDataType* data, size_t size);
+static void dma2_spi1_rx_tx_setup(uint8_t *tx, uint8_t *rx, size_t size);
+static void adxl345_read_xyz_dma(void);
+static void usart2_printf(const char *fmt, ...);
 static void setup_periphery(void);
 static void wait(uint32_t cycles);
-static void usart2_printf(const char *fmt, ...);
 
 ///
 /// Function definitions
@@ -46,64 +53,32 @@ void clock_setup(void)
 	rcc_periph_clock_enable(RCC_GPIOB);
 	rcc_periph_clock_enable(RCC_GPIOA);
 
-	// i2c
-	//rcc_periph_clock_enable(RCC_I2C1);
+	// spi
+    rcc_periph_clock_enable(RCC_SPI1);
 
 	// usart
 	rcc_periph_clock_enable(RCC_USART2);
 
 	// dma
 	rcc_periph_clock_enable(RCC_DMA1);
+	rcc_periph_clock_enable(RCC_DMA2);
 }
 
-/// @brief Setup dma
-/// @param data Address of data source
-/// @param size Number of data blocks
-void dma1_setup(DmaDataType* data, size_t size)
+/// @brief Setup gpios
+void gpio_setup(void)
 {
-	// enable interrupt
-	nvic_enable_irq(DMA_DEMO_STREAM_IRQ);
+	// gpio A5 (onboard led)
+	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
 
-	dma_stream_reset(DMA1, DMA_DEMO_STREAM);
-	dma_disable_stream(DMA1, DMA_DEMO_STREAM);
+	// usart2
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
+    gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
 
-	dma_set_priority(DMA1, DMA_DEMO_STREAM, DMA_SxCR_PL_HIGH);
-	dma_set_memory_size(DMA1, DMA_DEMO_STREAM, DMA_SxCR_MSIZE_8BIT);
-	dma_set_peripheral_size(DMA1, DMA_DEMO_STREAM, DMA_SxCR_PSIZE_8BIT);
-	dma_enable_memory_increment_mode(DMA1, DMA_DEMO_STREAM);
-
-	// dma mode
-#ifdef DMA_CIRCULAR
-	dma_enable_circular_mode(DMA1, DMA_DEMO_STREAM);
-#else
-	dma_enable_direct_mode(DMA1, DMA_DEMO_STREAM);
-#endif
-	dma_set_transfer_mode(DMA1, DMA_DEMO_STREAM, DMA_SxCR_DIR_MEM_TO_PERIPHERAL);
-
-	// dest address (usart)
-	dma_set_peripheral_address(DMA1, DMA_DEMO_STREAM, (uint32_t)&USART_DR(USART2));
-	
-	// src address 
-	dma_set_memory_address(DMA1, DMA_DEMO_STREAM, (uint32_t)data);
-	dma_set_number_of_data(DMA1, DMA_DEMO_STREAM, size);
-	dma_enable_transfer_complete_interrupt(DMA1, DMA_DEMO_STREAM);
-
-	dma_channel_select(DMA1, DMA_DEMO_STREAM, DMA_SxCR_CHSEL_4);
-	dma_enable_stream(DMA1, DMA_DEMO_STREAM);
-
-	usart_enable_tx_dma(USART2);
-}
-
-/// @brief Interrupt handler for dma1 stream 6
-void dma1_stream6_isr(void)
-{
-	if (dma_get_interrupt_flag(DMA1, DMA_DEMO_STREAM, DMA_TCIF)) 
-	{
-		dma_clear_interrupt_flags(DMA1, DMA_DEMO_STREAM, DMA_TCIF);
-		
-		// toggle led
-		gpio_toggle(GPIOA, GPIO5);
-	}
+	// spi1
+	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO5 | GPIO6 | GPIO7);
+    gpio_set_af(GPIOA, GPIO_AF5, GPIO5 | GPIO6 | GPIO7);
+    gpio_mode_setup(CS_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, CS_PIN);
+    gpio_set(CS_PORT, CS_PIN);
 }
 
 /// @brief Setup usart2
@@ -125,43 +100,53 @@ void usart2_setup(void)
 	nvic_enable_irq(DMA_DEMO_STREAM_IRQ);
 }
 
-/// @brief Setup gpios
-void gpio_setup(void)
+/// @brief Setup dma1 for usart2
+/// @param data Address of data source
+/// @param size Number of data blocks
+void dma1_usart2_setup(DmaDataType* data, size_t size)
 {
-	// gpio A5 (onboard led)
-	gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
+	// enable interrupt
+	nvic_enable_irq(DMA_DEMO_STREAM_IRQ);
 
-	// gpio A2 
-	gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO2);
-	gpio_set_af(GPIOA, GPIO_AF7, GPIO2);
+	dma_stream_reset(DMA1, DMA_DEMO_STREAM);
+	dma_disable_stream(DMA1, DMA_DEMO_STREAM);
+
+	dma_set_priority(DMA1, DMA_DEMO_STREAM, DMA_SxCR_PL_HIGH);
+	dma_set_memory_size(DMA1, DMA_DEMO_STREAM, DMA_SxCR_MSIZE_8BIT);
+	dma_set_peripheral_size(DMA1, DMA_DEMO_STREAM, DMA_SxCR_PSIZE_8BIT);
+	dma_enable_memory_increment_mode(DMA1, DMA_DEMO_STREAM);
+
+	// dma mode
+	dma_enable_direct_mode(DMA1, DMA_DEMO_STREAM);
+	dma_set_transfer_mode(DMA1, DMA_DEMO_STREAM, DMA_SxCR_DIR_MEM_TO_PERIPHERAL);
+
+	// dest address (usart)
+	dma_set_peripheral_address(DMA1, DMA_DEMO_STREAM, (uint32_t)&USART_DR(USART2));
+	
+	// src address 
+	dma_set_memory_address(DMA1, DMA_DEMO_STREAM, (uint32_t)data);
+	dma_set_number_of_data(DMA1, DMA_DEMO_STREAM, size);
+	dma_enable_transfer_complete_interrupt(DMA1, DMA_DEMO_STREAM);
+
+	dma_channel_select(DMA1, DMA_DEMO_STREAM, DMA_DEMO_CHANNEL);
+	dma_enable_stream(DMA1, DMA_DEMO_STREAM);
+
+	usart_enable_tx_dma(USART2);
 }
 
-/// @brief Setup spi1
-void spi1_setup(void)
+/// @brief Interrupt handler for dma1 stream 6
+void dma1_stream6_isr(void)
 {
-    rcc_periph_clock_enable(RCC_GPIOA);
-    rcc_periph_clock_enable(RCC_SPI1);
-
-    // SPI1: PA5=SCK, PA6=MISO, PA7=MOSI, PA4=CS
-    gpio_mode_setup(GPIOA, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO5 | GPIO6 | GPIO7);
-    gpio_set_af(GPIOA, GPIO_AF5, GPIO5 | GPIO6 | GPIO7);
-
-    // CS: PA4 als GPIO output
-    gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO4);
-    gpio_set(GPIOA, GPIO4); // CS high (deaktiviert)
-
-    //spi_reset(SPI1);
-    spi_init_master(SPI1, SPI_CR1_BAUDRATE_FPCLK_DIV_16,
-                    SPI_CR1_CPOL, SPI_CR1_CPHA,
-                    SPI_CR1_DFF_8BIT,
-                    SPI_CR1_MSBFIRST);
-    spi_enable(SPI1);
+	if (dma_get_interrupt_flag(DMA1, DMA_DEMO_STREAM, DMA_TCIF)) 
+	{
+		dma_clear_interrupt_flags(DMA1, DMA_DEMO_STREAM, DMA_TCIF);
+	}
 }
 
 /// @brief Format string with xyz values and send through dma (usart2)
 /// @param fmt 
 /// @param  
-void usart2_printf(const char *fmt, ...)
+void usart2_printf(const char *fmt, ...) 
 {
 	size_t maxStringLength = 24;
     DmaDataType buf[maxStringLength];
@@ -170,7 +155,98 @@ void usart2_printf(const char *fmt, ...)
     vsnprintf(buf, sizeof(buf), fmt, args);
     va_end(args);
 
-	dma1_setup(buf, maxStringLength);
+	dma1_usart2_setup(buf, maxStringLength);
+}
+
+/// @brief Setup spi1
+void spi1_setup(void)
+{
+    spi_init_master(SPI1, SPI_CR1_BAUDRATE_FPCLK_DIV_16,
+		SPI_CR1_CPOL, SPI_CR1_CPHA,
+		SPI_CR1_DFF_8BIT,
+		SPI_CR1_MSBFIRST);
+    spi_set_full_duplex_mode(SPI1);
+    spi_enable(SPI1);
+
+	nvic_set_priority(NVIC_DMA2_STREAM0_IRQ, 0);
+	nvic_enable_irq(NVIC_DMA2_STREAM0_IRQ);
+}
+
+/// @brief Setup dma2 for spi tx and rx
+/// @param tx Address of data source to be send through spi
+/// @param rx Address of data destination to be received through spi
+/// @param size 
+void dma2_spi1_rx_tx_setup(uint8_t *tx, uint8_t *rx, size_t size)
+{	
+	//
+    // RX
+	//
+	// enable interrupt
+	nvic_enable_irq(DMA_STREAM0);
+    dma_stream_reset(DMA2, DMA_STREAM0);
+    dma_channel_select(DMA2, DMA_STREAM0, DMA_SxCR_CHSEL_3);
+    dma_set_transfer_mode(DMA2, DMA_STREAM0, DMA_SxCR_DIR_PERIPHERAL_TO_MEM);
+    dma_set_peripheral_address(DMA2, DMA_STREAM0, (uint32_t)&SPI1_DR);
+    dma_set_memory_address(DMA2, DMA_STREAM0, (uint32_t)rx);
+    dma_set_number_of_data(DMA2, DMA_STREAM0, size);
+    dma_enable_memory_increment_mode(DMA2, DMA_STREAM0);
+    dma_disable_peripheral_increment_mode(DMA2, DMA_STREAM0);
+    dma_set_memory_size(DMA2, DMA_STREAM0, DMA_SxCR_MSIZE_8BIT);
+    dma_set_peripheral_size(DMA2, DMA_STREAM0, DMA_SxCR_PSIZE_8BIT);
+    dma_set_priority(DMA2, DMA_STREAM0, DMA_SxCR_PL_HIGH);
+	dma_enable_transfer_complete_interrupt(DMA2, DMA_STREAM0);
+
+	//
+    // TX
+	//
+    dma_stream_reset(DMA2, DMA_STREAM3);
+    dma_channel_select(DMA2, DMA_STREAM3, DMA_SxCR_CHSEL_3);
+    dma_set_transfer_mode(DMA2, DMA_STREAM3, DMA_SxCR_DIR_MEM_TO_PERIPHERAL);
+    dma_set_peripheral_address(DMA2, DMA_STREAM3, (uint32_t)&SPI1_DR);
+    dma_set_memory_address(DMA2, DMA_STREAM3, (uint32_t)tx);
+    dma_set_number_of_data(DMA2, DMA_STREAM3, size);
+    dma_enable_memory_increment_mode(DMA2, DMA_STREAM3);
+    dma_disable_peripheral_increment_mode(DMA2, DMA_STREAM3);
+    dma_set_memory_size(DMA2, DMA_STREAM3, DMA_SxCR_MSIZE_8BIT);
+    dma_set_peripheral_size(DMA2, DMA_STREAM3, DMA_SxCR_PSIZE_8BIT);
+    dma_set_priority(DMA2, DMA_STREAM3, DMA_SxCR_PL_HIGH);
+}
+
+/// @brief Global recieve buffer for spi 
+uint8_t rx[7];
+
+/// @brief Interrupt handler for dma2 stream 0 -> spi rx
+void dma2_stream0_isr(void)
+{
+	if (dma_get_interrupt_flag(DMA2, DMA_STREAM0, DMA_TCIF)) 
+	{
+		dma_clear_interrupt_flags(DMA2, DMA_STREAM0, DMA_TCIF);
+		dma_clear_interrupt_flags(DMA2, DMA_STREAM3, DMA_TCIF);
+
+		spi_disable_rx_dma(SPI1);
+		spi_disable_tx_dma(SPI1);
+		gpio_set(CS_PORT, CS_PIN);
+
+		int16_t x = (int16_t)((rx[2] << 8) | rx[1]);
+		int16_t y = (int16_t)((rx[4] << 8) | rx[3]);
+		int16_t z = (int16_t)((rx[6] << 8) | rx[5]);
+		usart2_printf("X=%4d, Y=%4d, Z=%4d\n", x, y, z);
+
+		__asm__("nop");
+	}
+}
+
+/// @brief Read sensor values through dma
+void adxl345_read_xyz_dma(void) 
+{
+    uint8_t tx[7] = { 0x80 | 0x40 | ADXL345_REG_DATA_X0, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+
+    dma2_spi1_rx_tx_setup(tx, rx, 7);
+    gpio_clear(CS_PORT, CS_PIN);
+    dma_enable_stream(DMA2, DMA_STREAM0);
+    dma_enable_stream(DMA2, DMA_STREAM3);
+    spi_enable_rx_dma(SPI1);
+    spi_enable_tx_dma(SPI1);
 }
 
 /// @brief Setup periphery
@@ -178,37 +254,25 @@ void setup_periphery(void)
 {
 	clock_setup();
 	gpio_setup();
-	spi1_setup();
-	usart2_setup();
-	//dma_setup(data, 1);
+    spi1_setup();
+    usart2_setup();
 
-	// sensor
+	// Initialize ADXL345 -> put it into measuring mode
 	adxl345_init(SPI1);
 }
 
 /// @brief Main function
 /// @return 0
-int main(void)
+int main(void) 
 {
 	setup_periphery();
 
-	int16_t x, y, z;
-	while (1)
+    while (1)
 	{
-		// toggle led
-		gpio_toggle(GPIOA, GPIO5);
+        adxl345_read_xyz_dma();
 
-		adxl345_read_xyz(SPI1, &x, &y, &z);
-
-		usart2_printf("X=%4d, Y=%4d, Z=%4d\n", x, y, z);
-
-		// dma write usart
-		//dma_setup(&data, 24);
-
-		wait(1000000);
-	}
-
-	return 0;
+        wait(1000000);
+    }
 }
 
 /// @brief Wait a given amount of cycles
